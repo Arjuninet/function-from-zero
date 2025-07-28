820c9dadd869ca51013d05ee184fe17c0b4c28a7
Arjuninet <133956968+Arjuninet@users.noreply.github.com>
2025-07-28 13:08:47 +0000

    test commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    """Returns the sum of a and b."""
+    return a + b
+
+
+print(add(3, 5))
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import add
+
+
+def test_add():
+    """Test the add function."""
+    assert add(3, 5) == 8
