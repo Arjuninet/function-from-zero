dd39df7589730dfaf0d1695fc885c94f7b60fe9b
Arjuninet <133956968+Arjuninet@users.noreply.github.com>
2025-07-29 08:46:29 +0000

    merge click code

--- wikibot.py
+++ wikibot.py
@@ -1,5 +1,12 @@
 import wikipedia
+import click
 
-def search_wikipedia(query="Facebook", length=1):
-    result = wikipedia.summary(query, sentences=length)
-    return result
+@click.command()
+@click.option('--name', prompt='Enter the topic name to scrape from Wikipedia',
+              help='Scrap on Wikipedia.')
+def search_wikipedia(name="Facebook", length=1):
+    result = wikipedia.summary(name, sentences=length)
+    click.echo(click.style(result, fg='yellow', bg='black', bold=True))
+
+if __name__ == "__main__":
+    search_wikipedia()
