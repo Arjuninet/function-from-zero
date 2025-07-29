ea5bc714d1e285e3ebf68b16609f9d0d2b1c401b
Arjuninet <133956968+Arjuninet@users.noreply.github.com>
2025-07-29 14:23:55 +0000

    CLI run code

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@ install:
 	pip install -r requirements.txt
 
 test:
-	pytest -s test_*.py
+	python -m pytest -s test_*.py
 
 format:
 	black *.py
 
 lint:
-	pylint --disable=R,C *.py
+	pylint --disable=R,C,no-value-for-parameter *.py
 
 all: install test format lint
--- test_wikibot.py
+++ test_wikibot.py
@@ -1,9 +1,9 @@
-from wikibot import search_wikipedia
+from wikibot import search_wikipedia, get_summary
 from click.testing import CliRunner
 
 
-# def test_search_wikipedia():
-#     assert "Microsoft" in get_summary("Microsoft")
+def test_search_wikipedia():
+    assert "Microsoft" in get_summary("Microsoft")
 
 
 def test_get_summary():
